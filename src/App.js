73b94aba5fa29a573125a2cdd9edd63d0d3ccce2
Christian Hamamdjian <christian.hamamdjian@welho.com>
2023-07-21 17:11:47 +0300

    New test with Q T curves

--- src/App.js
+++ src/App.js
@@ -19,3 +19,56 @@ function App() {
 }
 
 export default App;
+
+// import React, { useState } from 'react';
+
+// const App = () => {
+//   const [drawing, setDrawing] = useState(false);
+//   const [points, setPoints] = useState([]);
+//   console.log(points);
+//   const handleMouseDown = (event) => {
+//     setDrawing(true);
+//     setPoints((prevPoints) => [...prevPoints, { x: event.clientX, y: event.clientY }]);
+//   };
+
+//   const handleMouseMove = (event) => {
+//     if (drawing) {
+//       setPoints((prevPoints) => [...prevPoints, { x: event.clientX, y: event.clientY }]);
+//     }
+//   };
+
+//   const handleMouseUp = () => {
+//     setDrawing(false);
+//   };
+
+//   const renderPath = () => {
+//     if (points.length < 2) return null;
+
+//     let pathData = `M ${points[0].x} ${points[0].y}`;
+
+//     for (let i = 1; i < points.length - 1; i++) {
+//       const x = (points[i].x + points[i + 1].x) / 2;
+//       const y = (points[i].y + points[i + 1].y) / 2;
+//       pathData += ` Q ${points[i].x} ${points[i].y}, ${x} ${y}`;
+//     }
+
+//     pathData += ` T ${points[points.length - 1].x} ${points[points.length - 1].y}`;
+
+//     return <path d={pathData} fill="none" stroke="black" strokeWidth="3" />;
+//   };
+
+//   return (
+//     <svg
+//       width="500"
+//       height="500"
+//       style={{ border: '1px solid black' }}
+//       onPointerDown={handleMouseDown}
+//       onPointerMove={handleMouseMove}
+//       onPointerUp={handleMouseUp}
+//     >
+//       {renderPath()}
+//     </svg>
+//   );
+// };
+
+// export default App;
--- src/components/Features/Drawing.js
+++ src/components/Features/Drawing.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { MoodboardContext } from "../../context/moodboardContext";
-import svgPath from '../Helpers/pathSmooth';
+// import svgPath from '../Helpers/pathSmooth';
 
 const Drawing = () => {
     const { paths, selectedPath, handlePathClick, handlePathDrag, handlePathSelect } = React.useContext(MoodboardContext);
+
+    const renderPath = (points) => {
+        if (points.length < 2) return null
+
+        let pathData = `M ${points[0].x} ${points[0].y}`
+
+        for (let i = 1; i < points.length - 1; i++) {
+            const x = (points[i].x + points[i + 1].x) / 2
+            const y = (points[i].y + points[i + 1].y) / 2
+            pathData += ` Q ${points[i].x} ${points[i].y}, ${x} ${y}`
+        }
+
+        pathData += ` T ${points[points.length - 1].x} ${points[points.length - 1].y}`
+
+        return pathData
+    }
+
     return (
         <>
             {paths.map((path, index) => (
                 <path
                     key={index}
-                    d={svgPath(path["path"])} // Line smoothing to be fixed
-                    // d={`M${path["path"].map((point) => `${point.x} ${point.y}`).join(' L')}`}
+                    // d={svgPath(path["path"])} // Line smoothing to be fixed
+                    d={renderPath(path["path"])}
+                    //d={`M${path["path"].map((point) => `${point.x} ${point.y}`).join(' L')}`}
                     fill="none"
                     strokeLinecap="round"
                     strokeLinejoin="round"
