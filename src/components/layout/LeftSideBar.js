614416c4662040243e856403fb4ae08204d2950f
Christian Hamamdjian <christian.hamamdjian@welho.com>
2023-08-01 00:08:49 +0300

    Added left sidebar help, list styling fixes

--- src/components/layout/LeftSideBar.js
+++ src/components/layout/LeftSideBar.js
@@ -131,6 +131,15 @@ const LeftSidebard = () => {
                 </button>
                 {pdf && <PdfForm />}
             </div>
+            <div style={{ width: "160px", margin: "30px auto", overflowY: "auto", overflowX: "visible", listStylePosition: "inside" }}>
+                <h3>Help:</h3>
+                <p>Here are some tips about how to use <strong>Vible</strong>:</p>
+                <ol>
+                    <li>Add a drawring</li>
+                    <li>Add a text box</li>
+
+                </ol>
+            </div>
         </div>
     )
 }
