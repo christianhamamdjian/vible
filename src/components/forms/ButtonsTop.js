49d9640376f0fea24c2ef202f257f96e146f6d60
Christian Hamamdjian <christian.hamamdjian@welho.com>
2023-09-09 19:54:29 +0300

    Show active board number on boards button

--- src/components/forms/ButtonsTop.js
+++ src/components/forms/ButtonsTop.js
@@ -3,7 +3,7 @@ import Tooltips from '../tooltips/Tooltips'
 import { MoodboardContext } from "../../context/moodboardContext"
 
 const ButtonsTop = () => {
-    const { items, paths, isDrawing, isErasing, isPartialErasing, handlePartialEraser, isGrouping, handleGrouping, handleDrawing, handleEraser, handleEditingBoard, isEditingBoard, handleAddBox, handleInfo, handleShowBoards, info, showBoards } = React.useContext(MoodboardContext);
+    const { items, paths, isDrawing, isErasing, isPartialErasing, handlePartialEraser, isGrouping, handleGrouping, handleDrawing, handleEraser, handleEditingBoard, isEditingBoard, handleAddBox, handleInfo, handleShowBoards, info, showBoards, activeBoard } = React.useContext(MoodboardContext);
 
     return (
         <>
@@ -93,7 +93,6 @@ const ButtonsTop = () => {
                 />
                 {paths.length > 0 && (
                     <>
-
                         <button
                             className={isErasing ? "selected-button" : null}
                             title="Drag Eraser"
@@ -247,7 +246,7 @@ const ButtonsTop = () => {
                         className={showBoards ? "selected-button" : null}
                         title="Add and edit boards"
                         onClick={handleShowBoards}>
-                        <div className='boards-sign'>
+                        <div style={{ position: "relative" }} className='boards-sign'>
                             <svg
                                 x="0"
                                 y="0"
@@ -264,6 +263,7 @@ const ButtonsTop = () => {
                                     <path d="M23.991,5.16 C24.543,5.16 24.991,5.608 24.991,6.16 L24.991,11.917 C24.991,12.469 24.543,12.917 23.991,12.917 L18.106,12.917 C17.554,12.917 17.106,12.469 17.106,11.917 L17.106,6.16 C17.106,5.608 17.554,5.16 18.106,5.16 L23.991,5.16 z" fill="#ffffff" />
                                 </g>
                             </svg>
+                            <div style={{ width: "1rem", height: "1rem", backgroundColor: "#ffffff", borderRadius: "50%", color: "#3c3c3c", display: "flex", alignItems: "center", justifyContent: "center", position: "absolute", bottom: "-.1rem", right: "-.6rem" }}>{activeBoard.name}</div>
                         </div>
                     </button>
                     <Tooltips
