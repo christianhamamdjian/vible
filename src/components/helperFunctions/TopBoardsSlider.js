0cd4b48d111e81c1e2bce573c701e8984214e906
Christian Hamamdjian <christian.hamamdjian@welho.com>
2023-09-23 04:51:09 +0300

    Styling and icons

--- src/components/helperFunctions/TopBoardsSlider.js
+++ src/components/helperFunctions/TopBoardsSlider.js
@@ -64,13 +64,26 @@ const TopBoardsSlider = () => {
                 <label>Boards:</label>
                 <button
                     style={{
-                        backgroundColor: `${boardIndex === 0 ? "#ffffff" : "#8e8e93"}`,
-                        color: `${boardIndex === 0 ? "#ffffff" : "#ffffff"}`,
+                        backgroundColor: `${boardIndex === 0 ? "transparent" : "#8e8e93"}`,
+                        color: `${boardIndex === 0 ? "transparent" : "#ffffff"}`,
                     }}
                     onClick={() => handleBoardIndexUpdate(boardIndex - 1)}
                     className="inputs-boards_objects-nav"
                     disabled={boardIndex === 0}
-                >&lt;</button>
+                >
+                    <svg
+                        height="15"
+                        width="15"
+                        transform='rotate(180)'
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 320 512"
+                    >
+                        <path
+                            fill="currentColor"
+                            d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
+                        />
+                    </svg>
+                </button>
                 <button
                     className='inputs-boards_objects-button'
                     title="Add new board"
@@ -85,13 +98,25 @@ const TopBoardsSlider = () => {
                 {chunk}
                 <button
                     style={{
-                        backgroundColor: `${boardIndex === maxIndex ? "#ffffff" : "#8e8e93"} `,
-                        color: `${boardIndex === maxIndex ? "#ffffff" : "#ffffff"}`,
+                        backgroundColor: `${boardIndex === maxIndex ? "transparent" : "#8e8e93"} `,
+                        color: `${boardIndex === maxIndex ? "transparent" : "#ffffff"}`,
                     }}
                     onClick={() => handleBoardIndexUpdate(boardIndex + 1)}
                     className="inputs-boards_objects-nav"
                     disabled={boardIndex === maxIndex}
-                >&gt;</button>
+                >
+                    <svg
+                        height="15"
+                        width="15"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 320 512"
+                    >
+                        <path
+                            fill="currentColor"
+                            d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
+                        />
+                    </svg>
+                </button>
 
             </div>
             {onShow &&
