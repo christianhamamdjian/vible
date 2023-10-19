1e62c37f42ce1f22593d1c4030c3321466eeb9ed
Christian Hamamdjian <christian.hamamdjian@welho.com>
2023-10-20 01:39:06 +0300

    Updated the documentation disclamer

--- src/components/forms/VideoForm.js
+++ src/components/forms/VideoForm.js
@@ -7,10 +7,10 @@ const VideoForm = () => {
     return (<div className='itemForms'>
         <h2>Youtube video link:</h2>
         {/* <p>Example: https://www.youtube.com/embed/Dwvi_hjAS50 </p> */}
-        {/* <p>Just the video code: Dwvi_hjAS50 </p> */}
+        {/* <p>Just the video code example: Dwvi_hjAS50 </p> */}
         <form className='inputs' onSubmit={handleAddVideo}>
             <div className='inputs'>
-                <label>Add a Youtube video code like: Dwvi_hjAS50 </label>
+                <label>Add a Youtube video code like: I8ACAPIzG1g </label>
                 <input type="text" name="videourl" value={itemVideoUrl} onChange={handleItemVideoUrl} />
                 <button
                     className={`${!itemVideoUrl && "disabled"}`}
