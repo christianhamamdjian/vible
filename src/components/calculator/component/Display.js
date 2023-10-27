8d98f94328bd48981f83c2ce12f6321e7fcfe287
Christian Hamamdjian <christian.hamamdjian@welho.com>
2023-10-27 16:25:12 +0300

    Add calculator result to board

--- src/components/calculator/component/Display.js
+++ src/components/calculator/component/Display.js
@@ -1,9 +1,21 @@
 import React from "react"
+import { MoodboardContext } from "../../../context/moodboardContext"
 
 const Display = ({ value }) => {
+  const { handleTodoAddToBoard } = React.useContext(MoodboardContext);
   return (
     <div className="component-display">
-      <div>{value}</div>
+      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        <button
+          title="Add result to board"
+          className='todo-button'
+          onClick={() => handleTodoAddToBoard(value)}
+        >
+          <div className='todo-board-out'></div>
+          <div className='todo-board-in'>+</div>
+        </button>
+        {value}
+      </div>
     </div>
   )
 }
