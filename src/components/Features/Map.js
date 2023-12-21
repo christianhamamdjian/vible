8e8c9db82b37348571630745a01ae4c69a0b5298
Christian Hamamdjian <christian.hamamdjian@welho.com>
2023-12-21 04:57:39 +0200

    Fixed video and map editing

--- src/components/Features/Map.js
+++ src/components/Features/Map.js
@@ -20,94 +20,103 @@ const Map = ({ item }) => {
         <>
             {item.type === "mapUrl" && item.board === activeBoard.id &&
                 <>
-                    <g key={item.id}
-                        transform={`translate(${item.x},${item.y})`}
+                    <rect
+                        fill="#000000"
+                        x={item.x}
+                        y={isSafari ? item.y - 30 : item.y}
+                        width={item.width}
+                        height={item.height}
+                        rx="16"
+                        transform={`rotate(${item.angle || 0}, ${item.x + item.width / 2}, ${item.y + item.height / 2})`}
+                        onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
+                        onPointerMove={(e) => handleSvgPointerMove(e, item.id)}
+                        onPointerUp={(e) => handleSvgPointerUp(e, item.id)}
+                        onTouchStart={e => { handleSvgPointerDown(e, item.id) }}
+                        onTouchMove={(e) => handleSvgPointerMove(e, item.id)}
+                        onTouchEnd={(e) => handleSvgPointerUp(e, item.id)}
+                        // onDoubleClick={(e) => handleEditItem(e, item.id)}
+                        onClick={(e) => handleEditItem(e, item.id)}
+                    />
+                    <foreignObject
+                        x={isSafari ? "0" : item.x}
+                        y={isSafari ? "1rem" : item.y + 20}
+                        width={item.width}
+                        height={item.height}
+                        key={item.id}
+                        draggable="true"
+                        id="map-url"
+                        className='map-fobject'
+                        transform={`rotate(${item.angle || 0}, ${item.x + item.width / 2}, ${item.y + item.height / 2})`}
+                        style={{
+                            opacity: isDraggingRect && item.id === selectedRectId ? .8 : 1,
+                            top: isSafari ? "1rem" : item.y,
+                            left: isSafari ? item.x : "0",
+                        }}
+                        onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
+                        onPointerMove={(e) => handleSvgPointerMove(e, item.id)}
+                        onPointerUp={(e) => handleSvgPointerUp(e, item.id)}
+                        onTouchStart={e => { handleSvgPointerDown(e, item.id) }}
+                        onTouchMove={(e) => handleSvgPointerMove(e, item.id)}
+                        onTouchEnd={(e) => handleSvgPointerUp(e, item.id)}
+                        // onDoubleClick={(e) => handleEditItem(e, item.id)}
+                        onClick={(e) => handleEditItem(e, item.id)}
                     >
-                        <g
-                            className='map-object'
-                            transform={`rotate(${item.angle || 0}, ${item.width / 2}, ${item.height / 2})`}
+                        {!isSafari && <div
+                            xmlns="http://www.w3.org/1999/xhtml"
+                            className='map-isnotsafari'
                             style={{
-                                opacity: isDraggingRect && item.id === selectedRectId ? .8 : 1
+                                top: item.y,
+                                left: item.x,
+                                height: item.width * 9 / 12,
                             }}
-                            onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
-                            onPointerMove={(e) => handleSvgPointerMove(e, item.id)}
-                            onPointerUp={(e) => handleSvgPointerUp(e, item.id)}
-                            onTouchStart={(e) => handleSvgPointerDown(e, item.id)}
-                            onTouchMove={(e) => handleSvgPointerMove(e, item.id)}
-                            onTouchEnd={(e) => handleSvgPointerUp(e, item.id)}
-                            // onDoubleClick={(e) => handleEditItem(e, item.id)}
-                            onClick={(e) => handleEditItem(e, item.id)}
+                            onDoubleClick={(e) => handleEditItem(e, item.id)}
                         >
-                            {isEditingBoard && editingItem && editingItem.id === item.id && (
-                                <>
-                                    <circle
-                                        id="rotate"
-                                        fill="#cccccc"
-                                        cx="-20"
-                                        cy={`${item.height / 2}`}
-                                        width="20"
-                                        height="20"
-                                        r='12'
-                                        onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
-                                    />
-                                    <rect
-                                        id="resize"
-                                        fill="#cccccc"
-                                        x={item.width}
-                                        y={item.height}
-                                        width="20"
-                                        height="20"
-                                        rx="4"
-                                        onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
-                                    />
-                                </>)}
-                            {isSafari && <rect
-                                fill="#000000"
-                                x="0"
-                                y="-30"
-                                width={item.width}
-                                height={item.height}
-                                rx="16"
-                            />
-                            }
-                            <foreignObject
-                                x="0"
-                                y="0"
-                                width={item.width}
-                                height={item.height}
-                                draggable="true"
-                                id="map-url"
-                                className='map-fobject'
-                            >
-                                {!isSafari && <div
-                                    xmlns="http://www.w3.org/1999/xhtml"
-                                    className='map-isnotsafari'
-                                    style={{
-                                        top: item.y,
-                                        left: item.x,
-                                        height: item.width * 9 / 12,
-                                    }}
-                                    onDoubleClick={(e) => handleEditItem(e, item.id)}
-                                >
-                                </div>}
-                                <iframe
-                                    src={getUrl()}
-                                    width={item.width}
-                                    height={item.height}
-                                    className='map-frame'
-                                    allowFullScreen
-                                    loading="lazy"
-                                    referrerPolicy="no-referrer-when-downgrade"
-                                    style={{
-                                        transform: isSafari && `rotate(${item.angle || 0}deg)`,
-                                        top: isSafari ? item.y : "2rem",
-                                        left: isSafari ? item.x : "0",
-                                    }}
-                                >
-                                </iframe>
-                            </foreignObject>
-                            {isEditingBoard && <TopControls item={item} />}
-                        </g>
+                        </div>}
+                        <iframe
+                            src={getUrl()}
+                            width={item.width}
+                            height={item.height}
+                            className='map-frame'
+                            allowFullScreen
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                            style={{
+                                transform: isSafari && `rotate(${item.angle || 0}deg)`,
+                                top: isSafari ? item.y : "1rem",
+                                left: isSafari ? item.x : "0",
+                            }}
+                        >
+                        </iframe>
+                    </foreignObject>
+                    <g
+                        transform={`rotate(${item.angle || 0}, ${item.x + item.width / 2}, ${item.y + item.height / 2})`}
+                        style={{
+                            transform: `translate(${item.x},${item.y})`
+                        }}
+                    >
+                        {isEditingBoard && < TopControls item={item} />}{isEditingBoard && editingItem && editingItem.id === item.id && (
+                            <>
+                                <circle
+                                    id="rotate"
+                                    fill="#cccccc"
+                                    cx={item.x - 30}
+                                    cy={`${item.y + item.height / 2}`}
+                                    width="20"
+                                    height="20"
+                                    r='12'
+                                    onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
+                                />
+                                <rect
+                                    id="resize"
+                                    fill="#cccccc"
+                                    x={item.x + item.width}
+                                    y={item.y + item.height + 30}
+                                    width="20"
+                                    height="20"
+                                    rx="4"
+                                    onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
+                                />
+                            </>)}
                     </g>
                 </>
             }
--- src/components/Features/Video.js
+++ src/components/Features/Video.js
@@ -20,110 +20,109 @@ const Video = ({ item }) => {
                         }}
 
                     /> */}
-                    <g>
-                        <rect
-                            fill="#000000"
-                            x={item.x}
-                            y={isSafari ? item.y - 30 : item.y}
-                            width={item.width}
-                            height={item.height}
-                            rx="16"
-                            transform={`rotate(${item.angle || 0}, ${item.x + item.width / 2}, ${item.y + item.height / 2})`}
-                            onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
-                            onPointerMove={(e) => handleSvgPointerMove(e, item.id)}
-                            onPointerUp={(e) => handleSvgPointerUp(e, item.id)}
-                            onTouchStart={e => { handleSvgPointerDown(e, item.id) }}
-                            onTouchMove={(e) => handleSvgPointerMove(e, item.id)}
-                            onTouchEnd={(e) => handleSvgPointerUp(e, item.id)}
-                            // onDoubleClick={(e) => handleEditItem(e, item.id)}
-                            onClick={(e) => handleEditItem(e, item.id)}
-                        />
 
-                        <foreignObject
-                            id="video"
-                            x={isSafari ? "0" : item.x}
-                            y={isSafari ? "1rem" : item.y + 20}
-                            width={item.width}
-                            height={item.height}
-                            key={item.id}
-                            draggable="true"
-                            className='video-object'
-                            transform={`rotate(${item.angle || 0}, ${item.x + item.width / 2}, ${item.y + item.height / 2})`}
+                    <rect
+                        fill="#000000"
+                        x={item.x}
+                        y={isSafari ? item.y - 30 : item.y}
+                        width={item.width}
+                        height={item.height}
+                        rx="16"
+                        transform={`rotate(${item.angle || 0}, ${item.x + item.width / 2}, ${item.y + item.height / 2})`}
+                        onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
+                        onPointerMove={(e) => handleSvgPointerMove(e, item.id)}
+                        onPointerUp={(e) => handleSvgPointerUp(e, item.id)}
+                        onTouchStart={e => { handleSvgPointerDown(e, item.id) }}
+                        onTouchMove={(e) => handleSvgPointerMove(e, item.id)}
+                        onTouchEnd={(e) => handleSvgPointerUp(e, item.id)}
+                        // onDoubleClick={(e) => handleEditItem(e, item.id)}
+                        onClick={(e) => handleEditItem(e, item.id)}
+                    />
+
+                    <foreignObject
+                        id="video"
+                        x={isSafari ? "0" : item.x}
+                        y={isSafari ? "1rem" : item.y + 20}
+                        width={item.width}
+                        height={item.height}
+                        key={item.id}
+                        draggable="true"
+                        className='video-object'
+                        transform={`rotate(${item.angle || 0}, ${item.x + item.width / 2}, ${item.y + item.height / 2})`}
+                        style={{
+                            opacity: isDraggingRect && item.id === selectedRectId ? .8 : 1,
+                            top: isSafari ? "1rem" : item.y,
+                            left: isSafari ? item.x : "0",
+                        }}
+                        onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
+                        onPointerMove={(e) => handleSvgPointerMove(e, item.id)}
+                        onPointerUp={(e) => handleSvgPointerUp(e, item.id)}
+                        onTouchStart={e => { handleSvgPointerDown(e, item.id) }}
+                        onTouchMove={(e) => handleSvgPointerMove(e, item.id)}
+                        onTouchEnd={(e) => handleSvgPointerUp(e, item.id)}
+                        // onDoubleClick={(e) => handleEditItem(e, item.id)}
+                        onClick={(e) => handleEditItem(e, item.id)}
+                    >
+                        {!isSafari && <div
+                            xmlns="http://www.w3.org/1999/xhtml"
+                            className='video-isnotsafari'
                             style={{
-                                opacity: isDraggingRect && item.id === selectedRectId ? .8 : 1,
-                                top: isSafari ? "1rem" : item.y,
-                                left: isSafari ? item.x : "0",
+                                top: item.y,
+                                left: item.x,
+                                height: item.width * 9 / 12,
                             }}
-                            onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
-                            onPointerMove={(e) => handleSvgPointerMove(e, item.id)}
-                            onPointerUp={(e) => handleSvgPointerUp(e, item.id)}
-                            onTouchStart={e => { handleSvgPointerDown(e, item.id) }}
-                            onTouchMove={(e) => handleSvgPointerMove(e, item.id)}
-                            onTouchEnd={(e) => handleSvgPointerUp(e, item.id)}
-                            // onDoubleClick={(e) => handleEditItem(e, item.id)}
                             onClick={(e) => handleEditItem(e, item.id)}
                         >
-                            {!isSafari && <div
-                                xmlns="http://www.w3.org/1999/xhtml"
-                                className='video-isnotsafari'
-                                style={{
-                                    top: item.y,
-                                    left: item.x,
-                                    height: item.width * 9 / 12,
-                                }}
-                                onClick={(e) => handleEditItem(e, item.id)}
-                            >
-                            </div>}
-                            <iframe
-                                className='video-frame'
-                                style={{
-                                    transform: isSafari && `rotate(${item.angle || 0}deg)`,
-                                    top: isSafari ? item.y : "1rem",
-                                    left: isSafari ? item.x : "0",
-                                }}
-                                draggable="true"
-                                width="100%"
-                                height="100%"
-                                src={`https://www.youtube.com/embed/${item.videoUrl}`}
-                                title="YouTube video player"
-                                allow="accelerometer;
+                        </div>}
+                        <iframe
+                            className='video-frame'
+                            style={{
+                                transform: isSafari && `rotate(${item.angle || 0}deg)`,
+                                top: isSafari ? item.y : "1rem",
+                                left: isSafari ? item.x : "0",
+                            }}
+                            draggable="true"
+                            width="100%"
+                            height="100%"
+                            src={`https://www.youtube.com/embed/${item.videoUrl}`}
+                            title="YouTube video player"
+                            allow="accelerometer;
                             autoplay;
                             clipboard-write;
                             encrypted-media;
                             gyroscope;
                             picture-in-picture;"
-                            />
-                        </foreignObject>
-                        <g
-                            transform={`rotate(${item.angle || 0}, ${item.x + item.width / 2}, ${item.y + item.height / 2})`}
-                            style={{
-                                transform: `translate(${item.x},${item.y})`
-                            }}
-                        >
-                            {isEditingBoard && < TopControls item={item} />}{isEditingBoard && editingItem && editingItem.id === item.id && (
-                                <>
-                                    <circle
-                                        id="rotate"
-                                        fill="#cccccc"
-                                        cx={item.x - 30}
-                                        cy={`${item.y + item.height / 2}`}
-                                        width="20"
-                                        height="20"
-                                        r='12'
-                                        onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
-                                    />
-                                    <rect
-                                        id="resize"
-                                        fill="#cccccc"
-                                        x={item.x + item.width}
-                                        y={item.y + item.height + 30}
-                                        width="20"
-                                        height="20"
-                                        rx="4"
-                                        onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
-                                    />
-                                </>)}
-                        </g>
+                        />
+                    </foreignObject>
+                    <g
+                        transform={`rotate(${item.angle || 0}, ${item.x + item.width / 2}, ${item.y + item.height / 2})`}
+                        style={{
+                            transform: `translate(${item.x},${item.y})`
+                        }}
+                    >
+                        {isEditingBoard && < TopControls item={item} />}{isEditingBoard && editingItem && editingItem.id === item.id && (
+                            <>
+                                <circle
+                                    id="rotate"
+                                    fill="#cccccc"
+                                    cx={item.x - 30}
+                                    cy={`${item.y + item.height / 2}`}
+                                    width="20"
+                                    height="20"
+                                    r='12'
+                                    onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
+                                />
+                                <rect
+                                    id="resize"
+                                    fill="#cccccc"
+                                    x={item.x + item.width}
+                                    y={item.y + item.height + 30}
+                                    width="20"
+                                    height="20"
+                                    rx="4"
+                                    onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
+                                />
+                            </>)}
                     </g>
                 </>
             }
