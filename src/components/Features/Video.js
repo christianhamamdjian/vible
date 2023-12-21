10a66df1f6225f3ac6b7a0b935f8fd83aa367501
Christian Hamamdjian <christian.hamamdjian@welho.com>
2023-12-21 03:30:34 +0200

    Fixed video editing and handels

--- src/components/Features/Video.js
+++ src/components/Features/Video.js
@@ -10,15 +10,49 @@ const Video = ({ item }) => {
         <>
             {item.type === "video" && item.board === activeBoard.id &&
                 <>
-                    <g key={item.id}
-                        transform={`translate(${item.x},${item.y})`}
-                    >
-                        <g
+
+                    {/* <rect
+                        draggable="true"
+                        transform={`rotate(${item.angle || 0}, ${item.x + item.width / 2}, ${item.y + item.height / 2})`}
+                        className='video-group'
+                        style={{
+                            opacity: isDraggingRect && item.id === selectedRectId ? .8 : 1
+                        }}
+
+                    /> */}
+                    <g>
+                        <rect
+                            fill="#000000"
+                            x={item.x}
+                            y={isSafari ? item.y - 30 : item.y}
+                            width={item.width}
+                            height={item.height}
+                            rx="16"
+                            transform={`rotate(${item.angle || 0}, ${item.x + item.width / 2}, ${item.y + item.height / 2})`}
+                            onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
+                            onPointerMove={(e) => handleSvgPointerMove(e, item.id)}
+                            onPointerUp={(e) => handleSvgPointerUp(e, item.id)}
+                            onTouchStart={e => { handleSvgPointerDown(e, item.id) }}
+                            onTouchMove={(e) => handleSvgPointerMove(e, item.id)}
+                            onTouchEnd={(e) => handleSvgPointerUp(e, item.id)}
+                            // onDoubleClick={(e) => handleEditItem(e, item.id)}
+                            onClick={(e) => handleEditItem(e, item.id)}
+                        />
+
+                        <foreignObject
+                            id="video"
+                            x={isSafari ? "0" : item.x}
+                            y={isSafari ? "1rem" : item.y + 20}
+                            width={item.width}
+                            height={item.height}
+                            key={item.id}
                             draggable="true"
-                            transform={`rotate(${item.angle || 0}, ${item.width / 2}, ${item.height / 2})`}
-                            className='video-group'
+                            className='video-object'
+                            transform={`rotate(${item.angle || 0}, ${item.x + item.width / 2}, ${item.y + item.height / 2})`}
                             style={{
-                                opacity: isDraggingRect && item.id === selectedRectId ? .8 : 1
+                                opacity: isDraggingRect && item.id === selectedRectId ? .8 : 1,
+                                top: isSafari ? "1rem" : item.y,
+                                left: isSafari ? item.x : "0",
                             }}
                             onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
                             onPointerMove={(e) => handleSvgPointerMove(e, item.id)}
@@ -29,13 +63,50 @@ const Video = ({ item }) => {
                             // onDoubleClick={(e) => handleEditItem(e, item.id)}
                             onClick={(e) => handleEditItem(e, item.id)}
                         >
-                            {isEditingBoard && editingItem && editingItem.id === item.id && (
+                            {!isSafari && <div
+                                xmlns="http://www.w3.org/1999/xhtml"
+                                className='video-isnotsafari'
+                                style={{
+                                    top: item.y,
+                                    left: item.x,
+                                    height: item.width * 9 / 12,
+                                }}
+                                onClick={(e) => handleEditItem(e, item.id)}
+                            >
+                            </div>}
+                            <iframe
+                                className='video-frame'
+                                style={{
+                                    transform: isSafari && `rotate(${item.angle || 0}deg)`,
+                                    top: isSafari ? item.y : "1rem",
+                                    left: isSafari ? item.x : "0",
+                                }}
+                                draggable="true"
+                                width="100%"
+                                height="100%"
+                                src={`https://www.youtube.com/embed/${item.videoUrl}`}
+                                title="YouTube video player"
+                                allow="accelerometer;
+                            autoplay;
+                            clipboard-write;
+                            encrypted-media;
+                            gyroscope;
+                            picture-in-picture;"
+                            />
+                        </foreignObject>
+                        <g
+                            transform={`rotate(${item.angle || 0}, ${item.x + item.width / 2}, ${item.y + item.height / 2})`}
+                            style={{
+                                transform: `translate(${item.x},${item.y})`
+                            }}
+                        >
+                            {isEditingBoard && < TopControls item={item} />}{isEditingBoard && editingItem && editingItem.id === item.id && (
                                 <>
                                     <circle
                                         id="rotate"
                                         fill="#cccccc"
-                                        cx="-20"
-                                        cy={`${item.height / 2}`}
+                                        cx={item.x - 30}
+                                        cy={`${item.y + item.height / 2}`}
                                         width="20"
                                         height="20"
                                         r='12'
@@ -44,64 +115,14 @@ const Video = ({ item }) => {
                                     <rect
                                         id="resize"
                                         fill="#cccccc"
-                                        x={item.width}
-                                        y={item.height}
+                                        x={item.x + item.width}
+                                        y={item.y + item.height + 30}
                                         width="20"
                                         height="20"
                                         rx="4"
                                         onPointerDown={(e) => handleSvgPointerDown(e, item.id)}
                                     />
                                 </>)}
-                            {isSafari && <rect
-                                fill="#000000"
-                                x="0"
-                                y="-30"
-                                width={item.width}
-                                height={item.height}
-                                rx="16"
-                            />
-                            }
-                            <foreignObject
-                                id="video"
-                                x="0"
-                                y="0"
-                                width={item.width}
-                                height={item.height}
-                                draggable="true"
-                                className='video-object'
-                            >
-                                {!isSafari && <div
-                                    xmlns="http://www.w3.org/1999/xhtml"
-                                    className='video-isnotsafari'
-                                    style={{
-                                        top: item.y,
-                                        left: item.x,
-                                        height: item.width * 9 / 12,
-                                    }}
-                                    onDoubleClick={(e) => handleEditItem(e, item.id)}
-                                >
-                                </div>}
-                                <iframe
-                                    className='video-frame'
-                                    style={{
-                                        transform: isSafari && `rotate(${item.angle || 0}deg)`,
-                                        top: isSafari ? item.y : "2rem",
-                                        left: isSafari ? item.x : "0",
-                                    }}
-                                    draggable="true"
-                                    width="100%"
-                                    height="100%"
-                                    src={`https://www.youtube.com/embed/${item.videoUrl}`}
-                                    title="YouTube video player"
-                                    allow="accelerometer;
-                            autoplay;
-                            clipboard-write;
-                            encrypted-media;
-                            gyroscope;
-                            picture-in-picture;"
-                                />
-                            </foreignObject>
-                            {isEditingBoard && < TopControls item={item} />}
                         </g>
                     </g>
                 </>
--- src/components/helperComponents/TopControls.js
+++ src/components/helperComponents/TopControls.js
@@ -158,7 +158,7 @@ const TopControls = ({ item }) => {
                             // x={item.type === "box" ? item.x + 66 : "66"}
                             // y={item.type === "box" ? `${safariBrowser() ? item.y - 11 : item.y - 11}` : `${safariBrowser() ? - 41 : - 11}`}
                             x={item.x + 16}
-                            y={`${safariBrowser() ? item.y - 31 : item.y - 11}`}
+                            y={`${safariBrowser() ? item.y - 41 : item.y - 11}`}
                             width="24"
                             height="20"
                             fill="pink"
